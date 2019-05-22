1c978bbe9b4846cc4b15cbfa6cd14ab169b2f1d7
KhelifaSaifEddine <50869378+KhelifaSaifEddine@users.noreply.github.com>
2019-05-22 05:47:07 +0200

    Add files via upload

---
+++ App14/App14/App14/MainBefore.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Lottie.Forms;
+namespace App14
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainBefore : ContentPage
+	{
+		public MainBefore ()
+		{
+			InitializeComponent ();
+		}
+
+   async   private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage(),false);
+          
+        }
+    }
+}
---
+++ App14/App14/App14/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Lottie.Forms;
+
+namespace App14
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        
+
+           
+         
+        }
+
+     
+    }
+}
